1aad5cf0892a3b9289a0772b758888cd64fe8f48
Eugene7011 <e.podzirey@gmail.com>
2022-12-21 19:14:20 +0200

    Migrate to Postgres.

--- src/main/resources/db.migration/V1.0__Initial_Schema.sql
+++ src/main/resources/db.migration/V1.0__Initial_Schema.sql
@@ -2,19 +2,14 @@ CREATE TABLE "user"(
    "id" INTEGER PRIMARY KEY,
    "nickname" VARCHAR(45) NOT NULL,
    "email" VARCHAR(45) NOT NULL,
-   "password" VARCHAR(100) NOT NULL
+   "password" VARCHAR(100) NOT NULL,
+   "type" VARCHAR(45) NOT NULL default 'U'
 );
-
-ALTER TABLE "user" ADD COLUMN type VARCHAR(45) NOT NULL default 'U';
 
 CREATE TABLE "genre"(
     "id" INTEGER PRIMARY KEY,
     "name" VARCHAR(45) NOT NULL
 );
-
-CREATE SEQUENCE genres_id_sequence OWNED BY postgres.public.genre.id;
-ALTER TABLE genre ALTER COLUMN id SET DEFAULT nextval('genres_id_sequence');
-ALTER SEQUENCE genres_id_sequence INCREMENT BY 50;
 
 CREATE TABLE "movie"(
     "id" INTEGER PRIMARY KEY,
@@ -28,19 +23,10 @@ CREATE TABLE "movie"(
     "votes" INTEGER NOT NULL
 );
 
-CREATE SEQUENCE movies_id_sequence OWNED BY postgres.public.movie.id;
-ALTER TABLE "movie" ALTER COLUMN id SET DEFAULT nextval('movies_id_sequence');
-ALTER SEQUENCE movies_id_sequence INCREMENT BY 50;
-ALTER TABLE "movie" ALTER COLUMN votes SET DEFAULT '100';
-
 CREATE TABLE "country"(
   "id" INTEGER PRIMARY KEY,
   "name" VARCHAR(45) NOT NULL
 );
-
-CREATE SEQUENCE country_id_sequence OWNED BY postgres.public.country.id;
-ALTER TABLE country ALTER COLUMN id SET DEFAULT nextval('country_id_sequence');
-ALTER SEQUENCE country_id_sequence INCREMENT BY 50;
 
 CREATE TABLE "review"(
      "id" INTEGER PRIMARY KEY,
@@ -49,31 +35,44 @@ CREATE TABLE "review"(
      "description" VARCHAR(500) NOT NULL
 );
 
-CREATE SEQUENCE review_id_sequence OWNED BY postgres.public.review.id;
-ALTER TABLE review ALTER COLUMN id SET DEFAULT nextval('review_id_sequence');
-ALTER SEQUENCE review_id_sequence INCREMENT BY 50;
-
 CREATE TABLE "rating"(
      "user_id" INTEGER NOT NULL,
      "movie_id" INTEGER NOT NULL,
      "rating" DECIMAL(8, 1) NOT NULL
 );
 
-ALTER TABLE "rating" add CONSTRAINT "user_id" FOREIGN KEY("user_id") REFERENCES "user"("id");
-ALTER TABLE "rating" add CONSTRAINT "movie_id" FOREIGN KEY("movie_id") REFERENCES "movie"("id");
-
 CREATE TABLE "movie_genre"(
       "movie_id" INTEGER NOT NULL,
       "genre_id" INTEGER NOT NULL
 );
 
-ALTER TABLE "movie_genre" add CONSTRAINT "movie_id" FOREIGN KEY("movie_id") REFERENCES "movie"("id");
-ALTER TABLE "movie_genre" add CONSTRAINT "genre_id" FOREIGN KEY("genre_id") REFERENCES "genre"("id");
-
 CREATE TABLE "movie_country"(
         "movie_id" INTEGER NOT NULL,
         "country_id" INTEGER NOT NULL
 );
+
+CREATE SEQUENCE genres_id_sequence OWNED BY genre.id;
+ALTER TABLE "genre" ALTER COLUMN id SET DEFAULT nextval('genres_id_sequence');
+ALTER SEQUENCE genres_id_sequence INCREMENT BY 50;
+
+CREATE SEQUENCE movies_id_sequence OWNED BY movie.id;
+ALTER TABLE "movie" ALTER COLUMN id SET DEFAULT nextval('movies_id_sequence');
+ALTER SEQUENCE movies_id_sequence INCREMENT BY 50;
+ALTER TABLE "movie" ALTER COLUMN votes SET DEFAULT '100';
+
+CREATE SEQUENCE country_id_sequence OWNED BY country.id;
+ALTER TABLE "country" ALTER COLUMN id SET DEFAULT nextval('country_id_sequence');
+ALTER SEQUENCE country_id_sequence INCREMENT BY 50;
+
+CREATE SEQUENCE review_id_sequence OWNED BY review.id;
+ALTER TABLE "review" ALTER COLUMN id SET DEFAULT nextval('review_id_sequence');
+ALTER SEQUENCE review_id_sequence INCREMENT BY 50;
+
+ALTER TABLE "rating" add CONSTRAINT "user_id" FOREIGN KEY("user_id") REFERENCES "user"("id");
+ALTER TABLE "rating" add CONSTRAINT "movie_id" FOREIGN KEY("movie_id") REFERENCES "movie"("id");
+
+ALTER TABLE "movie_genre" add CONSTRAINT "movie_id" FOREIGN KEY("movie_id") REFERENCES "movie"("id");
+ALTER TABLE "movie_genre" add CONSTRAINT "genre_id" FOREIGN KEY("genre_id") REFERENCES "genre"("id");
 
 ALTER TABLE "movie_country" add CONSTRAINT "country_id" FOREIGN KEY("country_id") REFERENCES "country"("id");
 ALTER TABLE "movie_country" add CONSTRAINT "movie_id" FOREIGN KEY("movie_id") REFERENCES "movie"("id");
